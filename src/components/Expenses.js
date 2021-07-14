71b53f6e96ae5ef0436824da5534070e89b3eea2
Srikanth Gubba <gubbasrikanth2355@gmail.com>
2021-07-14 07:53:23 +0530

    components created(ExpensesFilter) and passing data from ExpensesFilter to Expenses(child to parent)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 import ExpenseItem from './ExpenseItem'
 import "./Expenses.css"
+import ExpensesFilter from './ExpensesFilter'
 
 function Expenses(props) {
+    const [selectYear, setselectYear] = useState('2020')
+    const filteredYear = (selectedyear) => {
+        setselectYear(selectedyear)
+    }
     return (
-        <Card className="expenses">
-            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
-            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
-            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
-            <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter defaultyear={selectYear} onFilterValue={filteredYear} />
+                <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
+                <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
+                <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
+                <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
 
-        </Card>
+            </Card>
+        </div>
+        
     )
 }
 
