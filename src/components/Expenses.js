03f566014ae82447379348147d64682e2b6cb896
Srikanth Gubba <gubbasrikanth2355@gmail.com>
2021-07-16 06:42:01 +0530

    ExpensesChart,Chart,ChartBar components are created

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import Card from './Card'
-import "./Expenses.css"
+ import "./Expenses.css"
 import ExpensesFilter from './ExpensesFilter'
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 
 function Expenses(props) {
     const [selectYear, setselectYear] = useState('2020')
@@ -17,6 +18,7 @@ function Expenses(props) {
         <div>
             <Card className="expenses">
                 <ExpensesFilter defaultyear={selectYear} onFilterValue={filteredYear} />
+                <ExpensesChart expenses={filteredItems} />
                 <ExpensesList expenses={filteredItems} />
             </Card>
         </div>
